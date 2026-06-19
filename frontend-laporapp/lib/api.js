12afba26c5eddad197520e281b838be22e8c85d3
saskyy <saskyyaa90@gmail.com>
2026-06-19 20:01:39 +0700

    YEAYY

--- frontend-laporapp/lib/api.js
+++ frontend-laporapp/lib/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://lapor-app-six.vercel.app";
 
 export async function daftarPengguna({ username, email, password }) {
   const res = await fetch(`${BASE_URL}/api/auth/register`, {
