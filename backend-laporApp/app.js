a5418c5c9dd7478ca0e432357db9c76be91cc8a1
saskyy <saskyyaa90@gmail.com>
2026-06-19 23:26:44 +0700

    FINISH 4

--- backend-laporApp/app.js
+++ backend-laporApp/app.js
@@ -15,8 +15,7 @@ import cors from "cors";
 const app = express()
 const port = 5000
 
-app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'] }));
-app.options('*', cors());
+app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], credentials: true }));
 app.use(express.json())
 app.use('/uploads', express.static('uploads'))
 app.use("/api/auth", authRouter)
