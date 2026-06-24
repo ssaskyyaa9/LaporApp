5b9c4e92936b3dbecd2a2c2452b4b2c439a609a3
saskyy <saskyyaa90@gmail.com>
2026-06-24 20:47:55 +0700

    ganti connection

--- backend-laporApp/database.js
+++ backend-laporApp/database.js
@@ -1,14 +1,19 @@
-import mysql from 'mysql2/promise'; 
+import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connection = await mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'db_laporApp',
-  port: process.env.DB_PORT || 3306
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
 });
 
 export default connection;
